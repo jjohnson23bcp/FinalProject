228dbbbafad352c1cd8711ed5a45c0cd0d882e8c
jjohnson23bcp <126900779+jjohnson23bcp@users.noreply.github.com>
2023-05-02 13:40:02 -0700

    libraries working (took a while)

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # file created by John Johnson
+
+
+############## open website with selenium and webdriver ###################
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://bcp.org'
+
+# get info from website
+res = requests.get(URL)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+# print(type(soup))
+# print(soup)
+elem = soup.select('div')
+# print(len(elem))
+print(str(elem))
+# dump into excel
