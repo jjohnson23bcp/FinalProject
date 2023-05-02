0366571f3c52d596151f8db7b76264d8113ff754
jjohnson23bcp <126900779+jjohnson23bcp@users.noreply.github.com>
2023-05-02 14:25:44 -0700

    commented soup scraper code

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # file created by John Johnson
 
-
 ############## open website with selenium and webdriver ###################
+# python library to make HTTP requests, access the internet
 import requests
+# python package for analyzing HTML and XML documents
 from bs4 import BeautifulSoup
 
 URL = 'https://bcp.org'
 
-# get info from website
+# get info from website, uses requests library to access the URL
 res = requests.get(URL)
+# shows HTTPError if there is one
 res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-# print(type(soup))
-# print(soup)
+# requesting text using BeautifulSoup, html.parser searches the document
+soup = BeautifulSoup(res.text, 'html.parser')
+# prints the type of object the variable soup is
+print(type(soup))
+# prints soup
+print(soup)
+# looking for html tag div
 elem = soup.select('div')
 # print(len(elem))
+# turns the html tag into strings
 print(str(elem))
 # dump into excel
