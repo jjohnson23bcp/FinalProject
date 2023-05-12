874ebb47f5bdf05833cd06c7b2be53347c2e55de
jjohnson23bcp <126900779+jjohnson23bcp@users.noreply.github.com>
2023-05-12 13:38:18 -0700

    working code

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ import requests
 # python package for analyzing HTML and XML documents
 from bs4 import BeautifulSoup
 # url that will be scraped
-# 'https://en.wikipedia.org/wiki/List_of_United_States_Military_Academy_alumni'
-URL = 'https://quotes.toscrape.com/'
+URL = 'https://en.wikipedia.org/wiki/List_of_United_States_Military_Academy_alumni'
 # get info from website, uses requests library to access the URL
 res = requests.get(URL)
-# shows HTTPError if there is one
-# res.raise_for_status()
 # requesting text using BeautifulSoup, html.parser searches the document
 soup = BeautifulSoup(res.text, 'html.parser')
-quotes = soup.find_all("span", attrs={"class":"text"})
+titles = soup.find_all("span", attrs={"class":"vcard"})
+descriptions = soup.find_all("td", attrs={"class":"note"})
+# printing  
+for title in titles:
+    print(title.text)
+for description in descriptions:
+    print(description.text)
 
-for quote in quotes:
-    print(quote.text)
+
